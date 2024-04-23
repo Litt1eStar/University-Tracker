37d7d742b71ede53244f1c92adc90fd52af8f812
Woody <devkritt@gmail.com>
2024-04-23 12:49:03 +0700

    Create: Connection to MongoDB

---
+++ server/connectDb.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connectDb = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log(`Connected To Database`))
+    .catch((e) => console.error(e));
+};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 
+import { connectDb } from './connectDb.js'
+
 const PORT = 5000
 const server = express()
 
 server.listen(PORT, ()=>{
+    connectDb()
     console.log(`Server running on PORT ${PORT}`)
 })
