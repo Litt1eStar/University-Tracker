84a7c309926b53cec4de7f52260a1c268076921a
Woody <devkritt@gmail.com>
2024-04-24 13:09:00 +0700

    I dont know

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
-import { Box } from '@mui/material'
-import './App.css'
+import { useState } from "react";
+import { Box, Button } from "@mui/material";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const res = await fetch("/api/university_year", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2Mjc1NGY5YTBlNDZhNzVkYmJmMGE2MiIsInVzZXJuYW1lIjoidXNlcjAzIiwiaWF0IjoxNzEzODU1NDg0fQ.2iAu-_vZLHMcoikYyKCuQHiwAvbeSuALNFlcP9YrLQo`,
+      },
+    });
+    const data = await res.json();
+    setData(data);
+  };
 
   return (
     <>
+      <Button onClick={fetchData}>Click Me!!</Button>
       <Box>
-        This is new Message
+        {data?.map((item) => (
+          <Box>{item.year}</Box>
+        ))}
       </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target: `http://${location.hostname}:5000`,
+        target: `http://localhost:8081`,
         secure: false
       }
     }
