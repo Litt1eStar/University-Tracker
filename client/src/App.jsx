238a638dd6f4cb66739b89bea13d22f33ac83234
Woody <devkritt@gmail.com>
2024-04-24 10:33:56 +0700

    Install React and Mui material

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Box>
+        Box
+      </Box>
+    </>
+  )
+}
+
+export default App
