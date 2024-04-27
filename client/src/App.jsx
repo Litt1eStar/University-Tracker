96f430c4140237c50045a92989ccef76604d6334
Woody <devkritt@gmail.com>
2024-04-27 20:56:34 +0700

    Initialize: react project with mui library and create react router for navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,21 @@
 import { useState } from "react";
-import { Box, Button } from "@mui/material";
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 import "./App.css";
 
-function App() {
-  const [data, setData] = useState([]);
-  const apiUrl = import.meta.env.VITE_API_URL;
-
-  const fetchData = async () => {
-    const res = await fetch(`${apiUrl}/api/university_year`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2Mjc1NGY5YTBlNDZhNzVkYmJmMGE2MiIsInVzZXJuYW1lIjoidXNlcjAzIiwiaWF0IjoxNzE0MTkwNjkyfQ.Gj_xe5KawMw6GlvGF6-D6A15ersJEHYzYeABQuzDyfY`,
-      },
-    });
-    const data = await res.json();
-    setData(data);    
-  };
-
+function App() {  
   return (
     <>
-      <Button onClick={fetchData}>Click Me!! Now</Button>
-      <Box>
-        {data?.map((item) => (
-          <Box>{item.year}</Box>
-        ))}
-      </Box>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />}/> 
+          <Route path="/signup" element={<SignUp />}/> 
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: ["Roboto Mono", "monospace"].join(","),
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
