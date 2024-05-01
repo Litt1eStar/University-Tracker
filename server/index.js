8c5c5d1b09e6a45aa6b556e59e110a6ef5058733
Woody <devkritt@gmail.com>
2024-05-01 12:04:56 +0700

    edit index.js

--- server/index.js
+++ server/index.js
@@ -19,9 +19,18 @@ const corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const app = express()
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use('/api/user', userRoute)
